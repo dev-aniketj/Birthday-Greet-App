024b53f4246363033bbdb9580bcd5e68decdb8b6
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-13 17:15:40 +0530

    master

---
+++ app/src/main/java/com/aniketjain/birthdaygreet/BirthdayGreetingActivity.kt
@@ -0,0 +1,14 @@
+package com.aniketjain.birthdaygreet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greeting)
+
+
+
+    }
+}
--- app/src/main/java/com/aniketjain/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/aniketjain/birthdaygreet/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.aniketjain.birthdaygreet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.aniketjain.birthdaygreet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        View Binding in Kt
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val name = binding.etInput.text.toString()
+
+        binding.btnCreateCard.setOnClickListener {
+            Toast.makeText(this, "My name is $name", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
